e15a1976cd2e88827fccbbb3385d0a130c8b6ab2
ekyeky <ekyeky1019@icloud.com>
2020-06-04 01:55:23 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html=lang="ja">
+    <head>
+        <meta charaset="utf-8">
+        <title>おみくじ</title>
+    </head>
+    
+    <body>
+        <h1>おみくじ</h1>
+        
+        <p>おみくじの結果：無し</p>
+        <form action="samle.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
+        </form>
+    </body>
+</html>
